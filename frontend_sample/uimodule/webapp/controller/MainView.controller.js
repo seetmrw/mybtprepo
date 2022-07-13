21f0245fd00a39f3260a5a9b25a1d2fddf9872d0
Jooyeon <seetmrw@naver.com>
2022-07-13 09:51:21 +0900

    commit!

--- frontend_sample/uimodule/webapp/controller/MainView.controller.js
+++ frontend_sample/uimodule/webapp/controller/MainView.controller.js
@@ -13,8 +13,8 @@ sap.ui.define(
         let oJson = new JSONModel();
         this.getView().setModel(oJson, 'myNode');
         oJson.loadData(
-            // 'http://localhost:8921/files'
-            'https://port-8921-nodejs-billowy-sandwich-seetmrw287880.codeanyapp.com/files'
+            'http://localhost:8921/files'
+            //'https://port-8921-nodejs-billowy-sandwich-seetmrw287880.codeanyapp.com/files'
         ).then(
             function () {
                 debugger;
