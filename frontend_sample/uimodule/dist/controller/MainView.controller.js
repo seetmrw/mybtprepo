c4669e2d0ab17693c22f6c5bebe4f678aebe35cf
Jooyeon <seetmrw@naver.com>
2022-07-16 22:30:03 +0900

    from VSCode Update

--- frontend_sample/uimodule/dist/controller/MainView.controller.js
+++
@@ -1,1 +0,0 @@
-sap.ui.define(["./BaseController","sap/ui/model/json/JSONModel"],function(e,t){"use strict";return e.extend("gitpg.myapp.controller.MainView",{onInit:function(){let e=new t;this.getView().setModel(e,"myNode");e.loadData("https://port-8921-nodejs-billowy-sandwich-seetmrw287880.codeanyapp.com/files").then(function(){debugger}.bind(this))}})});
