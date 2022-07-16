64369e0a39d68fbabe12ec2fdcbe8b57a968fe42
Jooyeon <seetmrw@naver.com>
2022-07-16 21:37:41 +0900

    commit!

--- frontend_sample/uimodule/webapp/controller/MainView.controller.js
+++ frontend_sample/uimodule/webapp/controller/MainView.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define(
   ["./BaseController",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment"
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, JSONModel) {
+  function (Controller, JSONModel, Fragment) {
     "use strict";
 
     return Controller.extend("gitpg.myapp.controller.MainView", {
@@ -13,8 +14,8 @@ sap.ui.define(
         let oJson = new JSONModel();
         this.getView().setModel(oJson, 'myNode');
         oJson.loadData(
-            // 'http://localhost:8921/files'
-            'https://port-8921-nodejs-billowy-sandwich-seetmrw287880.codeanyapp.com/files'
+            'http://localhost:8921/files'
+            // 'https://port-8921-nodejs-billowy-sandwich-seetmrw287880.codeanyapp.com/files'
         ).then(
             function () {
                 debugger;
@@ -32,6 +33,83 @@ sap.ui.define(
         //   }
         // )
       },
+      onPress:function(oEvent){
+        let sIdRaiseEvent = oEvent.getSource().getId();
+
+        let sIdLink0 = this.getView().byId('link0').getId();
+        let sIdLink1 = this.getView().byId('link1').getId();
+        let sIdLink2 = this.getView().byId('link2').getId();
+        let sIdLink3 = this.getView().byId('link3').getId();
+        let sIdLink4 = this.getView().byId('link4').getId();
+        let sIdLink5 = this.getView().byId('link5').getId();
+
+        let pFragment ;
+
+
+        if (sIdRaiseEvent === sIdLink0) {
+          pFragment = Fragment.load(
+            {
+              name : "gitpg.myapp.view.fragments.link0",
+              type : "XML",
+              id : "link0Fragment",
+              controller : this
+            }
+          )
+        } else if (sIdRaiseEvent === sIdLink1) {
+          pFragment = Fragment.load(
+            {
+              name : "gitpg.myapp.view.fragments.link1",
+              type : "XML",
+              id : "link1Fragment",
+              controller : this
+            }
+          )
+        } else if (sIdRaiseEvent === sIdLink2) {
+          pFragment = Fragment.load(
+            {
+              name : "gitpg.myapp.view.fragments.link2",
+              type : "XML",
+              id : "link2Fragment",
+              controller : this
+            }
+          )
+        } else if (sIdRaiseEvent === sIdLink3) {
+          pFragment = Fragment.load(
+            {
+              name : "gitpg.myapp.view.fragments.link3",
+              type : "XML",
+              id : "link3Fragment",
+              controller : this
+            }
+          )
+        } else if (sIdRaiseEvent === sIdLink4) {
+          pFragment = Fragment.load(
+            {
+              name : "gitpg.myapp.view.fragments.link4",
+              type : "XML",
+              id : "link4Fragment",
+              controller : this
+            }
+          )
+        } else if (sIdRaiseEvent === sIdLink5) {
+          pFragment = Fragment.load(
+            {
+              name : "gitpg.myapp.view.fragments.link5",
+              type : "XML",
+              id : "link5Fragment",
+              controller : this
+            }
+          )
+        }
+
+        pFragment.then(function (oView){
+          let oMyExtend = this.getView().byId('myExtend');
+          
+          oMyExtend.destroyItems();               //Remove items in myExtend
+          oMyExtend.addItem(oView);               //Add item
+
+        }.bind(this));
+      }
     });
   }
 );
